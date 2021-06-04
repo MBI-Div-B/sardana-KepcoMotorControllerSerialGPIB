ef3d1c567629b1108e4bf3ec0dbce6f057411bcf
Martin Hennecke <hennecke@mbi-berlin.de>
2021-06-04 12:26:40 +0200

    initial commit

---
+++ KepcoMotorControllerSerialGPIB.py
@@ -0,0 +1,150 @@
+import serial, time
+
+from sardana import State
+from sardana.pool.controller import MotorController
+from sardana.pool.controller import Type, Description, DefaultValue
+
+
+class KepcoMotorControllerSerialGPIB(MotorController):
+    ctrl_properties = {'Port': {Type: str, Description: 'ttyDevice', DefaultValue: '/dev/ttyKepco'}}
+    
+    MaxDevice = 1
+    
+    def __init__(self, inst, props, *args, **kwargs):
+        #super(kepcoController, self).__init__(
+        super(KepcoMotorControllerSerialGPIB, self).__init__(
+            inst, props, *args, **kwargs)
+
+        # connection settings
+        BAUDRATE = 115200
+        PARITY = serial.PARITY_NONE # serial.PARITY_NONE, serial.PARITY_ODD, serial.PARITY_EVEN
+        FLOWCONTROL = "none" # "none", "software", "hardware", "sw/hw"
+        TIMEOUT = 0
+        BYTESIZE = 8
+        STOPBITS = 1
+    
+        # configure serial
+        self.serial = serial.Serial()
+        self.serial.baudrate = BAUDRATE
+        self.serial.port = self.Port
+        self.serial.parity = PARITY
+        self.serial.bytesize = BYTESIZE
+        self.serial.stopbits = STOPBITS
+        self.serial.timeout = TIMEOUT
+
+        if FLOWCONTROL == "none":
+            self.serial.xonxoff = 0
+            self.serial.rtscts = 0
+        elif FLOWCONTROL == "software":
+            self.serial.xonxoff = 1
+            self.serial.rtscts = 0
+        elif FLOWCONTROL == "hardware":
+            self.serial.xonxoff = 0
+            self.serial.rtscts = 1
+        elif FLOWCONTROL == "sw/hw":
+            self.serial.xonxoff = 1
+            self.serial.rtscts = 1
+
+        # open serial port
+        self.serial.open()
+        
+        print ('Kepco Initialization')
+        self.serial.write("*IDN?\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        idn = self.serial.read_all()
+        idn = idn.decode("utf-8")
+
+        if idn:
+            print (idn)
+            print ('Kepco is initialized!')
+        else:
+            print ('Kepco is NOT initialized!')
+        # initialize hardware communication        
+        self._motors = {}
+        self._isMoving = None
+        self._moveStartTime = None
+        self._threshold = 0.1
+        self._target = None
+        self._timeout = 10
+
+    def AddDevice(self, axis):
+        self._motors[axis] = True
+        self.serial.write("FUNC:MODE CURR\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        self.serial.write("CURR:MODE FIX\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        self.serial.write("CURR:LIM:NEG 5\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        self.serial.write("CURR:LIM:POS 5\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        self.serial.write("OUTP ON\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        
+        res = self.serial.read_all()
+        res = res.decode("utf-8")
+
+    def DeleteDevice(self, axis):
+        
+        # close serial port
+        self.serial.close()
+        
+        del self._motors[axis]
+
+    def StateOne(self, axis):
+        limit_switches = MotorController.NoLimitSwitch
+        pos = self.ReadOne(axis)
+        now = time.time()
+        
+        if self._isMoving == False:
+            state = State.On
+        elif self._isMoving:
+            if (abs(pos-self._target) > self._threshold):
+                # moving and not in threshold window
+                if (now-self._moveStartTime) < self._timeout:
+                    # before timeout
+                    state = State.Moving
+                else:
+                    # after timeout
+                    self._log.warning('Kepco Timeout')
+                    self._isMoving = False
+                    state = State.On
+            elif (abs(pos-self._target) <= self._threshold):
+                self._isMoving = False
+                state = State.On
+        else:
+            state = State.Fault
+        
+        return state, 'some text', limit_switches
+
+    def ReadOne(self, axis):
+        self.serial.write("MEAS:CURR?\n".encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        res = self.serial.read_all()
+        res = res.decode("utf-8")
+        return float(res)
+
+    def StartOne(self, axis, position):
+        self._moveStartTime = time.time()
+        self._isMoving = True
+        self._target = position
+        cmd = 'CURR {:f}'.format(position)+'\n'
+        self.serial.write(cmd.encode("utf-8"))
+        #self.serial.flush()
+        time.sleep(0.05)
+        res = self.serial.read_all()
+        res = res.decode("utf-8")
+
+    def StopOne(self, axis):
+        pass
+
+    def AbortOne(self, axis):
+        pass
+
+    
--- Readme.md
+++ Readme.md
@@ -1,13 +1,27 @@
-# Template for a Tango Device Server
-This is a template for what your Readme for a Tango Device Server should look like. This first abstract should summarize what your Tango DS is for.
-Direct further documentation of classes or methods used to the <em>docs</em> folder. Make sure the manuals you upload to <em>manuals</em> are freely accessible. 
+# sardana-KepcoMotorControllerSerialGPIB
+This a Sardana MotorController for controlling Kepco BOP power supplies using Serial-to-GPIB-adapters. It uses the standard command strings needed for controlling a Kepco power supply, but sends them to a serial ttyDevice instead of a real GPIB port.
 
-## Installation
-Describe how to install necessary packages (maybe drivers) needed for your Tango DS. Maybe it is needed to install by <code>pip3</code> or clone and install from another git.
+# Installation
+## USB Serial converter
+Create a udev rule in order to mount the USB Serial converter always under the same link, e.g. ```/dev/ttyPIMercury```.
 
-## Configuration
-(Optional)  
-Add some configuration details that need to be set up when registering the Server (which device properties to declare etc.).
+First check the VendorID, ProductID, and SerialNumber using ```dmesg```. Then add a new udev rule:
+```
+sudo nano /etc/udev/rules.d/55-usbcom.rules
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="PX4UALD3", SYMLINK+="ttyKepco", MODE="0666"
+```
+Reload and apply the udev rule by
+```
+sudo udevadm control --reload
+sudo udevadm trigger --action=add
+```
+
+## Serial-to-GPIB adapter
+The Sardana controller does not allow to configure parameters specific to the GPIB protocol. This has to be done before in the respective Serial-to-GPIB adapter, which usually allow to set the GPIB address and other parameters via serial control strings or the a configuration utility provided by the manufacturer. Don't forget to save the parameters in the adapters non-volatile memory.
+
+## Sardana Configuration
+The ```KepcoMotorControllerSerialGPIB``` Class requires to set the following properties:
+- Port (e.g., ```/dev/ttyKepco```)
 
 ## Authors
-Refer to your organization and leave some contact details of how to reach you in case there are any questions. 
+Martin Hennecke
